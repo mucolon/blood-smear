5f6cc071f834b48f076c56283198611f377f4e9e
Marco U. Colón <44480595+mucolon@users.noreply.github.com>
2019-04-26 01:29:38 -0700

    Add files via upload

---
+++ test/basic_smear.py
@@ -0,0 +1,11 @@
+# This is the test code for a basic smear
+#
+# run program with this line of code below
+# sudo python basic_smear.py
+
+# importing libraries
+from BBpystepper import Stepper
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.PWM as PWM
+
+# configuring pins
