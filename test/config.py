cf0b4dbcdc4eb5a3f52752477f303c722bf1ccb1
Marco U. Colón <44480595+mucolon@users.noreply.github.com>
2019-04-26 19:31:18 -0700

    Update config.py

--- test/config.py
+++ test/config.py
@@ -7,7 +7,7 @@ from array import *
 
 # example GPIO pins
 # active pins for linear guide
-slide_pins = {"pul": "P8_17", "dir": "P8_15", "ena": "P8_16"}
+slide_pins = {"pul": "P9_41", "dir": "P9_23", "ena": "P9_15"}
 
 # active pins for near limit switch
 # near_pins = {"1": "P8_17", "2": "P8_15"}
