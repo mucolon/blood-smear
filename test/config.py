094fe7fed6116adfeed2f2347e601f552d7e4960
Marco U. Colón <44480595+mucolon@users.noreply.github.com>
2019-04-28 19:20:07 -0700

    Add files via upload

--- test/config.py
+++ test/config.py
@@ -2,11 +2,11 @@
 
 
 # importing libraries
-from array import *
+# from array import *
 
 
 # active pins for linear guide
-slide_pins = {"pul": "P9_41", "dir": "P9_23", "ena": "P9_15"}
+slide_pins = {"ena": "P8_11", "dir": "P8_15", "pul": "P8_17"}
 
 # active pins for near limit switch
 # near_pins = {"1": "P8_17", "2": "P8_15"}
--- test/slide_stepper.py
+++ test/slide_stepper.py
@@ -8,18 +8,18 @@
 # importing libraries
 import Adafruit_BBIO.GPIO as GPIO
 import time
-import math
-import config
+# import math
+# import config
 
 
 # function to step motor forward
-def step_forward(pins, pin_index):
-    GPIO.output(config.slide_pins["pul"], GPIO.LOW)
+def step_forward(pins):
+    GPIO.output(pins["pul"], GPIO.HIGH)
 
 
 # function to step motor backward
-def step_backward(pins, pin_index):
-    GPIO.output(config.slide_pins["pul"], GPIO.LOW)
+def step_backward(pins):
+    GPIO.output(pins["pul"], GPIO.HIGH)
 
 
 # declaring stepper class with functions
@@ -27,39 +27,39 @@ class Slide(object):
 
     # function to initialize pins
     def init_pins(self, pins):
-        GPIO.setup(config.slide_pins["pul"], GPIO.OUT)
-        GPIO.setup(config.slide_pins["dir"], GPIO.OUT)
-        GPIO.setup(config.slide_pins["ena"], GPIO.OUT)
-        GPIO.output(config.slide_pins["pul"], GPIO.HIGH)
-        GPIO.output(config.slide_pins["ena"], GPIO.LOW)
+        GPIO.setup(pins["pul"], GPIO.OUT)
+        GPIO.setup(pins["dir"], GPIO.OUT)
+        GPIO.setup(pins["ena"], GPIO.OUT)
+        GPIO.output(pins["pul"], GPIO.LOW)
+        GPIO.output(pins["ena"], GPIO.LOW)
 
-        # fucntion to move motor clockwise
+    # fucntion to move motor clockwise
     def spin_clockwise(self, pins, rotations, rpm):
         sleep_time = 0.3 / float(rpm)
-        steps_forward = rotations * 200  # 1 micro step = 200 pulses
-        # steps_forward = rotations * 6400 # 32 micro steps = 6400 pulses
-        GPIO.output(config.slide_pins["dir"], GPIO.HIGH)
-        for x in range(0, steps_forward):
+        steps = rotations * 200  # 1 micro step = 200 pulses
+        # steps = rotations * 6400 # 32 micro steps = 6400 pulses
+        GPIO.output(pins["dir"], GPIO.HIGH)
+        for x in range(0, steps):
             step_forward(self, pins)
             time.sleep(sleep_time)
-            GPIO.output(config.slide_pins["pul"], GPIO.HIGH)
+            GPIO.output(pins["pul"], GPIO.LOW)
 
-        # fucntion to move motor counterclockwise
+    # fucntion to move motor counterclockwise
     def spin_counterclockwise(self, pins, rotations, rpm):
         sleep_time = 0.3 / float(rpm)
-        steps_backward = rotations * 200  # 1 micro step = 200 pulses
-        # steps_background = rotations * 6400 # 32 micro steps = 6400 pulses
-        GPIO.output(config.slide_pins["dir"], GPIO.LOW)
-        for x in range(0, steps_backward):
+        steps = rotations * 200  # 1 micro step = 200 pulses
+        # steps = rotations * 6400 # 32 micro steps = 6400 pulses
+        GPIO.output(pins["dir"], GPIO.LOW)
+        for x in range(0, steps):
             step_backward(self, pins)
             time.sleep(sleep_time)
-            GPIO.output(config.slide_pins["pul"], GPIO.HIGH)
+            GPIO.output(pins["pul"], GPIO.LOW)
 
-        # function to cleanup up pins from use
+    # function to cleanup up pins from use
     def cleanup(self, pins):
         GPIO.cleanup()
 
-        # function to disable motor from sending pulses
+    # function to disable motor from sending pulses
     def disable(self, pins):
-        GPIO.output(config.slide_pins["ena"], GPIO.HIGH)
-        GPIO.output(config.slide_pins["pul"], GPIO.LOW)
+        GPIO.output(pins["ena"], GPIO.HIGH)
+        GPIO.output(pins["pul"], GPIO.LOW)
