af582f9d08e9eece2589565852695387de75afb3
Marco U. Colón <44480595+mucolon@users.noreply.github.com>
2019-05-07 16:01:14 -0700

    Add files via upload

--- test/test_servo.py
+++ test/test_servo.py
@@ -14,24 +14,35 @@ import config
 def main():
     while True:
         try:
-            angle = str(input("\nEnter servo motor angle (n to exit): "))
+            # angle = str(input("\nEnter servo motor angle (n to exit): "))
+            duty = str(input("\nEnter servo duty cycle [0-100] (n to exit): "))
         except ValueError:
             print("Error: Invalid Input")
             continue
-        if angle == "n":
+        # if angle == "n":
+        if duty == "n":
             break
-        angle_f = float(angle)
-        servo.change_angle(angle_f)
-        print("Current servo angle: ", angle_f)
+        # angle_f = float(angle)
+        # servo.change_angle(angle_f)
+        duty = int(duty)
+        servo.change_duty(duty)
+        # print("Current servo angle: ", angle_f)
+        print("Cureent servo duty cycle: ", duty)
 
 
 if __name__ == "__main__":
 
     # initializing  classes
     servo = Servo(config.unload_pin)
+    near_switch = Input_io(config.limit_near_pin, "fall")
+    far_switch = Input_io(config.limit_far_pin, "fall")
+    slide = Stepper(config.slide_pins)
 
     # initializing pins
-    servo.start(5, 10, 50)
+    servo.start(5, 10, 50, 0)
+    near_switch.init_pin()
+    far_switch.init_pin()
+    slide.init_pins()
 
     # confirming power
     input("Press any key after motors are connected to power")
@@ -40,3 +51,6 @@ if __name__ == "__main__":
 
     print("\nClosing Program")
     servo.cleanup()
+    near_switch.cleanup()
+    far_switch.cleanup()
+    slide.cleanup()
