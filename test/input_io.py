283e39c67ac398a62411ba38af41ee29c087dc55
Marco U. Colón <44480595+mucolon@users.noreply.github.com>
2019-05-13 20:02:03 -0700

    Add files via upload

--- test/input_io.py
+++ test/input_io.py
@@ -42,7 +42,7 @@ class Input_io():
     def init_pin(self, func, freq = 1):
         # function retruns: sig - beaglebone input pin
         #                   edge - pin edge detection
-        bouncetime = float(1E3/freq)
+        bouncetime = int(1E3/freq)
         GPIO.setup(self.sig, GPIO.IN, self.resistor)
         GPIO.add_event_detect(self.sig, self.edge, func, bouncetime)
         return (self.sig, self.edge)
